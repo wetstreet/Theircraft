20f64bf846fa167125116963eecaf1a5b90bcab4
Chen Yirun 陈奕润 <chenyirun1996@yahoo.com>
2018-11-17 09:44:06 +0800

    add preview tool

---
+++ Assets/Editor/Custom/EditorTools.cs
@@ -0,0 +1,20 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+public class EditorTools
+{
+
+    [MenuItem("Assets/Preview UI Prefab")]
+    public static void PreviewUIPrefab()
+    {
+        GameObject prefab = Selection.activeGameObject;
+        if (prefab != null)
+        {
+            UISystem.InstantiatePrefab(prefab);
+        }
+    }
+}
+
+#endif
