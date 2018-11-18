76bed9759b12dcdae8812174db5837047e3822b2
Chen Yirun 陈奕润 <chenyirun1996@yahoo.com>
2018-11-18 12:54:54 +0800

    改进后的生成方块方式

--- Assets/Scripts/BlockGenerator.cs
+++ Assets/Scripts/BlockGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.IO;
+using UnityEditor;
 
 public enum BlockType
 {
@@ -51,99 +52,24 @@ public static class BlockGenerator {
     static Vector3 far_right_top = new Vector3(0.5f, 0.5f, 0.5f);
     static Vector3 far_left_top = new Vector3(-0.5f, 0.5f, 0.5f);
 
-    public static GameObject CreateCube(BlockType blockType)
-    {
-        GameObject cube = new GameObject("cube");
-        cube.AddComponent<BoxCollider>();
-
-        BlockTexture bt = BlockTexture.type2texture[blockType];
-
-        AddFace(cube, FaceType.TopFace, bt.TopTexture);
-        AddFace(cube, FaceType.BottomFace, bt.BottomTexture);
-        AddFace(cube, FaceType.FrontFace, bt.FrontTexture);
-        AddFace(cube, FaceType.BackFace, bt.BackTexture);
-        AddFace(cube, FaceType.LeftFace, bt.LeftTexture);
-        AddFace(cube, FaceType.RightFace, bt.RightTexture);
-
-        return cube;
-    }
-
-    static void AddFace(GameObject cube, FaceType faceType, string blockTexturePath)
-    {
-        GameObject face = CreateFace(faceType, blockTexturePath);
-        face.transform.parent = cube.transform;
-    }
-
-    static GameObject CreateFace(FaceType faceType, string blockTexturePath)
-    {
-        GameObject face = new GameObject("face");
-        MeshFilter mf = face.AddComponent<MeshFilter>();
-        MeshRenderer mr = face.AddComponent<MeshRenderer>();
-        mr.material = LoadMaterial(blockTexturePath);
-        mf.mesh = CreateFaceMesh(faceType);
-        return face;
-    }
-
-    public static Texture LoadTexture(string texturePath)
-    {
-        Texture t = Resources.Load<Texture>(texturePath);
-        if (t.filterMode != FilterMode.Point)
-            t.filterMode = FilterMode.Point;
-        return t;
-    }
-
-    public static Texture LoadBlockTexture(string blockTexturePath)
-    {
-        return LoadTexture("textures/blocks/" + blockTexturePath);
-    }
-
-    static Material LoadMaterial(string blockTexturePath)
-    {
-        Material m = new Material(Shader.Find("Unlit/Texture"));
-        m.mainTexture = LoadBlockTexture(blockTexturePath);
-        return m;
-    }
-
-    static Mesh CreateFaceMesh(FaceType faceType)
-    {
-        Vector3[] vertex = { };
-        Vector2[] uv = { new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1) };
-        int[] triangles = { 0, 2, 1, 0, 3, 2 };
-
-        switch (faceType)
-        {
-            case FaceType.FrontFace:
-                vertex = new Vector3[] { near_left_bottom, near_right_bottom, near_right_top, near_left_top };
-                break;
-            case FaceType.BackFace:
-                vertex = new Vector3[] { far_right_bottom, far_left_bottom, far_left_top, far_right_top };
-                break;
-            case FaceType.LeftFace:
-                vertex = new Vector3[] { far_left_bottom, near_left_bottom, near_left_top, far_left_top };
-                break;
-            case FaceType.RightFace:
-                vertex = new Vector3[] { near_right_bottom, far_right_bottom, far_right_top, near_right_top };
-                break;
-            case FaceType.TopFace:
-                vertex = new Vector3[] { near_left_top, near_right_top, far_right_top, far_left_top };
-                break;
-            case FaceType.BottomFace:
-                vertex = new Vector3[] { far_left_bottom, far_right_bottom, near_right_bottom, near_left_bottom };
-                break;
-        }
-
-        Mesh m = new Mesh();
-        m.vertices = vertex;
-        m.uv = uv;
-        m.triangles = triangles;
-        return m;
-    }
-    #region 动态合并texture测试
-
-
     static List<Vector3> vertex = new List<Vector3>();
     static List<Vector2> uv = new List<Vector2>();
     static List<int> triangles = new List<int>();
+
+    static Dictionary<string, Texture2D> path2texture = new Dictionary<string, Texture2D>();
+    static Dictionary<BlockType, MeshAndMaterial> blockType2MeshAndMaterial = new Dictionary<BlockType, MeshAndMaterial>();
+
+    struct MeshAndMaterial
+    {
+        public Mesh mesh;
+        public Material material;
+        public MeshAndMaterial(Mesh _mesh, Material _material)
+        {
+            mesh = _mesh;
+            material = _material;
+        }
+    }
+
     static void AddFace(FaceType faceType, Rect rect)
     {
         Vector2 uv_left_bottom = new Vector2(rect.x, rect.y);
@@ -176,41 +102,169 @@ public static class BlockGenerator {
         }
     }
 
-    public static Texture2D[] textures;
-    static void GenerateBlock(BlockType blockType)
+    // 读取材质并缓存起来
+    static Texture2D GetBlockTexture(string path)
     {
-        Texture2D atlas = new Texture2D(512, 512);
-        Rect[] rects = atlas.PackTextures(textures, 0, 512);
-        Material material = Resources.Load<Material>("New Material 2");
-        //material.mainTexture = atlas;
-        byte[] bytes = atlas.EncodeToPNG();
-        string path = Directory.GetCurrentDirectory() + "/Assets/Resources/atlas.png";
-        using (FileStream file = File.Open(path, FileMode.Create))
+        if (!path2texture.ContainsKey(path))
         {
-            BinaryWriter writer = new BinaryWriter(file);
-            writer.Write(bytes);
+            Texture2D tex = Resources.Load<Texture2D>("textures/blocks/" + path);
+            if (tex != null)
+                path2texture[path] = tex;
+        }
+        return path2texture[path];
+    }
+
+    // 根据blocktype获得该方块的贴图列表（可能重复）
+    static Texture2D[] GetTexturesByBlockType(BlockType blockType)
+    {
+        BlockTexture bt = BlockTexture.type2texture[blockType];
+        Texture2D texTop = GetBlockTexture(bt.TopTexture);
+        Texture2D texBottom = GetBlockTexture(bt.BottomTexture);
+        Texture2D texFront = GetBlockTexture(bt.FrontTexture);
+        Texture2D texBack = GetBlockTexture(bt.BackTexture);
+        Texture2D texLeft = GetBlockTexture(bt.LeftTexture);
+        Texture2D texRight = GetBlockTexture(bt.RightTexture);
+        Texture2D[] textures = new Texture2D[] { texTop, texBottom, texFront, texBack, texLeft, texRight };
+
+        foreach (Texture2D tex in textures)
+        {
+            TextureImporter ti = (TextureImporter)AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(tex));
+            ti.isReadable = true;
+            ti.filterMode = FilterMode.Point;
+            ti.textureCompression = TextureImporterCompression.Uncompressed;
+            AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(tex));
         }
 
-        vertex.Clear();
-        uv.Clear();
-        triangles.Clear();
-
-        AddFace(FaceType.TopFace, rects[0]);
-        AddFace(FaceType.BottomFace, rects[2]);
-        AddFace(FaceType.FrontFace, rects[1]);
-        AddFace(FaceType.BackFace, rects[1]);
-        AddFace(FaceType.LeftFace, rects[1]);
-        AddFace(FaceType.RightFace, rects[1]);
-
-        Mesh mesh = new Mesh();
-        mesh.vertices = vertex.ToArray();
-        mesh.uv = uv.ToArray();
-        mesh.triangles = triangles.ToArray();
-
-
-        //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        //cube.GetComponent<MeshFilter>().mesh = mesh;
-        //cube.GetComponent<MeshRenderer>().material = 
+        return textures;
     }
-    #endregion
+
+    static public GameObject CreateCube(BlockType blockType)
+    {
+        if (!blockType2MeshAndMaterial.ContainsKey(blockType))
+        {
+            Texture2D[] textures = GetTexturesByBlockType(blockType);
+            Texture2D combinedTex = new Texture2D(128, 128);
+            Rect[] rects = combinedTex.PackTextures(textures, 0, 128);
+            combinedTex.filterMode = FilterMode.Point;
+
+            vertex.Clear();
+            uv.Clear();
+            triangles.Clear();
+
+            AddFace(FaceType.TopFace, rects[0]);
+            AddFace(FaceType.BottomFace, rects[1]);
+            AddFace(FaceType.FrontFace, rects[2]);
+            AddFace(FaceType.BackFace, rects[3]);
+            AddFace(FaceType.LeftFace, rects[4]);
+            AddFace(FaceType.RightFace, rects[5]);
+
+            Mesh mesh = new Mesh();
+            mesh.vertices = vertex.ToArray();
+            mesh.uv = uv.ToArray();
+            mesh.triangles = triangles.ToArray();
+
+            Material material = new Material(Shader.Find("Unlit/Texture"));
+            material.mainTexture = combinedTex;
+
+            blockType2MeshAndMaterial[blockType] = new MeshAndMaterial(mesh, material);
+        }
+
+        MeshAndMaterial mam = blockType2MeshAndMaterial[blockType];
+
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        MeshFilter mf = cube.GetComponent<MeshFilter>();
+        mf.mesh = mam.mesh;
+        Renderer renderer = cube.GetComponent<Renderer>();
+        renderer.material = mam.material;
+
+        return cube;
+    }
+
+    //public static GameObject CreateCube(BlockType blockType)
+    //{
+    //    GameObject cube = new GameObject("cube");
+    //    cube.AddComponent<BoxCollider>();
+
+    //    BlockTexture bt = BlockTexture.type2texture[blockType];
+
+    //    AddFace(cube, FaceType.TopFace, bt.TopTexture);
+    //    AddFace(cube, FaceType.BottomFace, bt.BottomTexture);
+    //    AddFace(cube, FaceType.FrontFace, bt.FrontTexture);
+    //    AddFace(cube, FaceType.BackFace, bt.BackTexture);
+    //    AddFace(cube, FaceType.LeftFace, bt.LeftTexture);
+    //    AddFace(cube, FaceType.RightFace, bt.RightTexture);
+
+    //    return cube;
+    //}
+
+    //static void AddFace(GameObject cube, FaceType faceType, string blockTexturePath)
+    //{
+    //    GameObject face = CreateFace(faceType, blockTexturePath);
+    //    face.transform.parent = cube.transform;
+    //}
+
+    //static GameObject CreateFace(FaceType faceType, string blockTexturePath)
+    //{
+    //    GameObject face = new GameObject("face");
+    //    MeshFilter mf = face.AddComponent<MeshFilter>();
+    //    MeshRenderer mr = face.AddComponent<MeshRenderer>();
+    //    mr.material = LoadMaterial(blockTexturePath);
+    //    mf.mesh = CreateFaceMesh(faceType);
+    //    return face;
+    //}
+
+    //public static Texture LoadTexture(string texturePath)
+    //{
+    //    Texture t = Resources.Load<Texture>(texturePath);
+    //    if (t.filterMode != FilterMode.Point)
+    //        t.filterMode = FilterMode.Point;
+    //    return t;
+    //}
+
+    //public static Texture LoadBlockTexture(string blockTexturePath)
+    //{
+    //    return LoadTexture("textures/blocks/" + blockTexturePath);
+    //}
+
+    //static Material LoadMaterial(string blockTexturePath)
+    //{
+    //    Material m = new Material(Shader.Find("Unlit/Texture"));
+    //    m.mainTexture = LoadBlockTexture(blockTexturePath);
+    //    return m;
+    //}
+
+    //static Mesh CreateFaceMesh(FaceType faceType)
+    //{
+    //    Vector3[] vertex = { };
+    //    Vector2[] uv = { new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1) };
+    //    int[] triangles = { 0, 2, 1, 0, 3, 2 };
+
+    //    switch (faceType)
+    //    {
+    //        case FaceType.FrontFace:
+    //            vertex = new Vector3[] { near_left_bottom, near_right_bottom, near_right_top, near_left_top };
+    //            break;
+    //        case FaceType.BackFace:
+    //            vertex = new Vector3[] { far_right_bottom, far_left_bottom, far_left_top, far_right_top };
+    //            break;
+    //        case FaceType.LeftFace:
+    //            vertex = new Vector3[] { far_left_bottom, near_left_bottom, near_left_top, far_left_top };
+    //            break;
+    //        case FaceType.RightFace:
+    //            vertex = new Vector3[] { near_right_bottom, far_right_bottom, far_right_top, near_right_top };
+    //            break;
+    //        case FaceType.TopFace:
+    //            vertex = new Vector3[] { near_left_top, near_right_top, far_right_top, far_left_top };
+    //            break;
+    //        case FaceType.BottomFace:
+    //            vertex = new Vector3[] { far_left_bottom, far_right_bottom, near_right_bottom, near_left_bottom };
+    //            break;
+    //    }
+
+    //    Mesh m = new Mesh();
+    //    m.vertices = vertex;
+    //    m.uv = uv;
+    //    m.triangles = triangles;
+    //    return m;
+    //}
 }
