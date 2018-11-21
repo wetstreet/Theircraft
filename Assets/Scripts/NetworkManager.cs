b20314e6f66b2964f03c994edb61a49e3e4ab364
Chen Yirun 陈奕润 <chenyirun1996@yahoo.com>
2018-11-21 23:44:05 +0800

    自测服务器时客户端的操作

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -40,7 +40,12 @@ public static class NetworkManager
 
     public static bool connected;
 
-    static readonly string ip = "39.108.139.67";
+    static readonly string PUBLIC_SERVER_IP = "39.108.139.67";
+    static readonly string LOCAL_SERVER_IP = "10.0.4.254";
+
+    //自测服务器的改动时，把ip指向LOCAL_SERVER_IP，并把LOCAL_SERVER_IP改为本地服务器启动后第一行输出的ip（每台电脑都不一样）
+    //这个ip的改动不要提交，否则别的客户端连不上外网服务器
+    static readonly string ip = PUBLIC_SERVER_IP;
     static readonly int port = 8848;
 
     static TcpClient tcpClient;
