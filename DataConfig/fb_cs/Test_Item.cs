bacc77fa0e814602eb88bdcdc0a56a6df04a7005
Roshan <870536460@qq.com>
2018-11-27 20:33:00 +0800

    edit flatbuffer bash

--- DataConfig/fb_cs/Test_Item.cs
+++ DataConfig/fb_cs/Test_Item.cs
@@ -5,14 +5,14 @@
 using global::System;
 using global::FlatBuffers;
 
-public struct test_Item : IFlatbufferObject
+public struct Test_Item : IFlatbufferObject
 {
   private Table __p;
   public ByteBuffer ByteBuffer { get { return __p.bb; } }
-  public static test_Item GetRootAstest_Item(ByteBuffer _bb) { return GetRootAstest_Item(_bb, new test_Item()); }
-  public static test_Item GetRootAstest_Item(ByteBuffer _bb, test_Item obj) { return (obj.__assign(_bb.GetInt(_bb.Position) + _bb.Position, _bb)); }
+  public static Test_Item GetRootAsTest_Item(ByteBuffer _bb) { return GetRootAsTest_Item(_bb, new Test_Item()); }
+  public static Test_Item GetRootAsTest_Item(ByteBuffer _bb, Test_Item obj) { return (obj.__assign(_bb.GetInt(_bb.Position) + _bb.Position, _bb)); }
   public void __init(int _i, ByteBuffer _bb) { __p.bb_pos = _i; __p.bb = _bb; }
-  public test_Item __assign(int _i, ByteBuffer _bb) { __init(_i, _bb); return this; }
+  public Test_Item __assign(int _i, ByteBuffer _bb) { __init(_i, _bb); return this; }
 
   public int ID { get { int o = __p.__offset(4); return o != 0 ? __p.bb.GetInt(o + __p.bb_pos) : (int)0; } }
   public string Name { get { int o = __p.__offset(6); return o != 0 ? __p.__string(o + __p.bb_pos) : null; } }
@@ -31,27 +31,27 @@ public struct test_Item : IFlatbufferObject
 #endif
   public byte[] GetDescArray() { return __p.__vector_as_array<byte>(10); }
 
-  public static Offset<test_Item> Createtest_Item(FlatBufferBuilder builder,
+  public static Offset<Test_Item> CreateTest_Item(FlatBufferBuilder builder,
       int ID = 0,
       StringOffset NameOffset = default(StringOffset),
       double Money = 0.0,
       StringOffset DescOffset = default(StringOffset)) {
     builder.StartObject(4);
-    test_Item.AddMoney(builder, Money);
-    test_Item.AddDesc(builder, DescOffset);
-    test_Item.AddName(builder, NameOffset);
-    test_Item.AddID(builder, ID);
-    return test_Item.Endtest_Item(builder);
+    Test_Item.AddMoney(builder, Money);
+    Test_Item.AddDesc(builder, DescOffset);
+    Test_Item.AddName(builder, NameOffset);
+    Test_Item.AddID(builder, ID);
+    return Test_Item.EndTest_Item(builder);
   }
 
-  public static void Starttest_Item(FlatBufferBuilder builder) { builder.StartObject(4); }
+  public static void StartTest_Item(FlatBufferBuilder builder) { builder.StartObject(4); }
   public static void AddID(FlatBufferBuilder builder, int ID) { builder.AddInt(0, ID, 0); }
   public static void AddName(FlatBufferBuilder builder, StringOffset NameOffset) { builder.AddOffset(1, NameOffset.Value, 0); }
   public static void AddMoney(FlatBufferBuilder builder, double Money) { builder.AddDouble(2, Money, 0.0); }
   public static void AddDesc(FlatBufferBuilder builder, StringOffset DescOffset) { builder.AddOffset(3, DescOffset.Value, 0); }
-  public static Offset<test_Item> Endtest_Item(FlatBufferBuilder builder) {
+  public static Offset<Test_Item> EndTest_Item(FlatBufferBuilder builder) {
     int o = builder.EndObject();
-    return new Offset<test_Item>(o);
+    return new Offset<Test_Item>(o);
   }
 };
 
