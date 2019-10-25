7b4a75a297a57ee9aef8542f20e69d0ae6263206
Chen Yirun 陈奕润 <chenyirun1996@yahoo.com>
2019-10-25 20:55:06 +0800

    arrange files

---
+++ Assets/Scripts/BasicSystem/Chunk/Chunk.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using protocol.cs_theircraft;
+
+public class Chunk
+{
+    public int x;
+    public int z;
+    public Vector2Int pos;      //used for index in dictionary
+    public Transform transform;
+    public GameObject gameObject;
+    public byte[] blocksInByte;
+
+    MeshFilter meshFilter;
+    MeshCollider meshCollider;
+
+    public Chunk()
+    {
+        GenerateGameObject();
+    }
+
+    public void SetData(int _x, int _z, byte[] _blocksInByte)
+    {
+        x = _x;
+        z = _z;
+        pos.x = x;
+        pos.y = z;
+        blocksInByte = _blocksInByte;
+        gameObject.name = "chunk (" + x + "," + z + ")";
+    }
+
+    public static int GetChunkPosByGlobalPos(int _x)
+    {
+        return Mathf.FloorToInt(_x / 16f);
+    }
+
+    public int GetXInChunkByGlobalX(int _x)
+    {
+        return _x - x * 16;
+    }
+
+    public int GetZInChunkByGlobalZ(int _z)
+    {
+        return _z - z * 16;
+    }
+
+    //input is local position
+    public CSBlockType GetBlockType(int x, int y, int z)
+    {
+        return (CSBlockType)blocksInByte[256 * y + 16 * x + z];
+    }
+
+    //input is local position
+    public void SetBlockType(int x, int y, int z, CSBlockType type)
+    {
+        blocksInByte[256 * y + 16 * x + z] = (byte)type;
+    }
+
+    //input is local position
+    public bool HasBlock(int x, int y, int z)
+    {
+        if (x < 0 || x > 15 || z < 0 || z > 15 || y < 0 || y > 255)
+        {
+            return false;
+        }
+        return GetBlockType(x, y, z) != CSBlockType.None;
+    }
+
+    public void RebuildMesh()
+    {
+        Mesh mesh = ChunkMeshGenerator.GenerateMesh(this);
+        meshFilter.sharedMesh = mesh;
+        meshCollider.sharedMesh = mesh;
+    }
+
+    public void ClearData()
+    {
+        meshFilter.sharedMesh = null;
+        meshCollider.sharedMesh = null;
+    }
+
+    public void GenerateGameObject()
+    {
+        if (gameObject != null)
+        {
+            return;
+        }
+
+        gameObject = new GameObject("chunk (" + x + "," + z + ")");
+        transform = gameObject.transform;
+        meshFilter = gameObject.AddComponent<MeshFilter>();
+
+        MeshRenderer mr = gameObject.AddComponent<MeshRenderer>();
+        mr.material = Resources.Load<Material>("merge-test/block");
+
+        meshCollider = gameObject.AddComponent<MeshCollider>();
+        gameObject.tag = "Chunk";
+        gameObject.layer = LayerMask.NameToLayer("Chunk");
+    }
+}
---
+++ Assets/Scripts/BasicSystem/Chunk/ChunkPool.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkPool
+{
+    static Queue<Chunk> chunks = new Queue<Chunk>(1000);
+    static GameObject instance;
+    static GameObject chunkParent;
+
+    public static void Init()
+    {
+        instance = new GameObject("ChunkPool");
+        chunkParent = new GameObject("Chunks");
+        instance.transform.localPosition = new Vector3(0, -100, 0);
+        for (int i = 0; i < 1000; i++)
+        {
+            Chunk chunk = new Chunk();
+            chunk.transform.parent = instance.transform;
+            chunks.Enqueue(chunk);
+        }
+    }
+
+    public static Chunk GetChunk()
+    {
+        Chunk chunk = chunks.Dequeue();
+        chunk.transform.parent = chunkParent.transform;
+        chunk.transform.localPosition = Vector3.zero;
+        return chunk;
+    }
+
+    public static void Recover(Chunk chunk)
+    {
+        chunk.transform.parent = instance.transform;
+        chunk.transform.localPosition = Vector3.zero;
+        chunks.Enqueue(chunk);
+    }
+}
