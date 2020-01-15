408c54e13f9b2f49308baf60aac2561d144914c5
Chen Yirun 陈奕润 <chenyirun1996@yahoo.com>
2020-01-15 20:16:56 +0800

    better navigation

--- client/Assets/Scripts/LogicSystem/Player.cs
+++ client/Assets/Scripts/LogicSystem/Player.cs
@@ -14,8 +14,17 @@ public class Player
     public Transform transform;
     private Transform steve;
     Transform head;
-    private NavMeshAgent agent;
     private CharacterController cc;
+    private NavMeshPath path;
+
+    private int currTargetIndex;
+
+    public float speed = 5f;
+    public float jumpSpeed = 9;
+    public float gravity = 28;
+    Vector3 moveDir = Vector3.zero;
+
+    public float angularSpeed = 300f;
 
     public Player(CSPlayer p)
     {
@@ -24,24 +33,68 @@ public class Player
         transform.name = "player_" + p.PlayerID;
         steve = transform.Find("steve");
         head = transform.Find("steve/Armature/Move/Body_Lower/Body_Upper/Head.001");
-        agent = transform.GetComponent<NavMeshAgent>();
         cc = transform.GetComponent<CharacterController>();
+        path = new NavMeshPath();
 
         Move(p.Position, p.Rotation);
     }
 
     public void SetDestination(Vector3 pos)
     {
-        agent.destination = pos;
+        if (Physics.Raycast(transform.position + transform.up / 2, -transform.up, out RaycastHit playerHit))
+        {
+            if (NavMesh.CalculatePath(playerHit.point, pos, NavMesh.AllAreas, path))
+            {
+                currTargetIndex = 1;
+            }
+        }
     }
 
     public void Update()
     {
-        if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, 1f, LayerMask.GetMask("Chunk")))
+        Vector3 horizontalDir = Vector3.zero;
+        if (path.corners.Length > 0 && currTargetIndex != path.corners.Length)
         {
-            float dis = Vector3.Distance(transform.position, hit.point);
-            steve.transform.localPosition = new Vector3(0, -dis, 0);
+            Vector3 player2target = path.corners[currTargetIndex] - transform.position;
+            horizontalDir = new Vector3(player2target.x, 0, player2target.z);
+            float dis = horizontalDir.sqrMagnitude;
+            if (dis < 0.005f)
+            {
+                if (currTargetIndex < path.corners.Length - 1)
+                {
+                    currTargetIndex++;
+                    player2target = path.corners[currTargetIndex] - transform.position;
+                    horizontalDir = new Vector3(player2target.x, 0, player2target.z);
+                }
+                else
+                {
+                    horizontalDir = Vector3.zero;
+                }
+            }
+
+            if (horizontalDir != Vector3.zero)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(horizontalDir, Vector3.up), angularSpeed * Time.deltaTime);
+            }
         }
+        horizontalDir = horizontalDir.normalized;
+        horizontalDir *= speed;
+
+        moveDir = new Vector3(horizontalDir.x, moveDir.y, horizontalDir.z);
+
+        if (cc.isGrounded)
+        {
+            moveDir.y = 0;
+
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                moveDir.y = jumpSpeed;
+            }
+        }
+
+        moveDir.y -= gravity * Time.deltaTime;
+
+        cc.Move(moveDir * Time.deltaTime);
     }
 
     public void Move(CSVector3 pos, CSVector3 rot)
