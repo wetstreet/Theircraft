bd40981a9e9e5e5ec8af64d3b85b81e30c8a0706
Chen Yirun 陈奕润 <chenyirun1996@yahoo.com>
2020-01-17 21:47:39 +0800

    better break effect (each particle different uv)

--- client/Assets/Scripts/LogicSystem/BreakBlockEffect.cs
+++ client/Assets/Scripts/LogicSystem/BreakBlockEffect.cs
@@ -1,44 +1,62 @@
 ﻿using protocol.cs_theircraft;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BreakBlockEffect : MonoBehaviour
 {
-    static Dictionary<CSBlockType, string> type2breakEffect = new Dictionary<CSBlockType, string>
+    static Dictionary<CSBlockType, Vector2Int> type2breakEffect = new Dictionary<CSBlockType, Vector2Int>
     {
-        {CSBlockType.Grass, "dirt" },
-        {CSBlockType.Dirt, "dirt" },
-        {CSBlockType.Tnt, "tnt_side" },
-        {CSBlockType.Brick, "brick" },
-        {CSBlockType.Furnace, "furnace_front_off" },
-        {CSBlockType.HayBlock, "hay_block_side" },
+        {CSBlockType.GrassBlock, ChunkMeshGenerator.grass_bottom },
+        {CSBlockType.Dirt, ChunkMeshGenerator.grass_bottom },
+        {CSBlockType.Tnt, ChunkMeshGenerator.tnt_side },
+        {CSBlockType.Brick, ChunkMeshGenerator.brick },
+        {CSBlockType.Furnace, ChunkMeshGenerator.furnace_side },
+        {CSBlockType.HayBlock, ChunkMeshGenerator.hay_side },
     };
 
-    public static void Create(CSBlockType type,Vector3Int pos)
+    public static void Create(CSBlockType type, Vector3Int pos)
     {
         GameObject prefab = Resources.Load("Prefabs/BreakBlockEffect") as GameObject;
-        GameObject effect = Instantiate(prefab);
-        effect.transform.localPosition = pos;
-        Destroy(effect, 1);
-        
+        GameObject go = Instantiate(prefab);
+        go.transform.localPosition = pos;
+        Destroy(go, 1);
+
+        BreakBlockEffect effect = go.AddComponent<BreakBlockEffect>();
+        Vector2Int uv = Vector2Int.zero;
         if (type2breakEffect.ContainsKey(type))
         {
-            string name = type2breakEffect[type];
-            Texture tex = Resources.Load<Texture>("GUI/PlainBlock/" + name);
-            effect.GetComponent<Renderer>().material.mainTexture = tex;
+            uv = type2breakEffect[type];
         }
+        else
+        {
+            uv = ChunkMeshGenerator.type2texcoords[(byte)type].front;
+        }
+        effect.x = uv.x;
+        effect.y = uv.y;
     }
 
+    Texture2D effectTexture;
+    public int x = 0;
+    public int y = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        if (effectTexture == null)
+        {
+            effectTexture = new Texture2D(16, 16);
+            effectTexture.filterMode = FilterMode.Point;
+        }
+        Texture2D blockTexture = Resources.Load<Material>("Materials/block").mainTexture as Texture2D;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < 16; i++)
+        {
+            for (int j = 0; j < 16; j++)
+            {
+                Color c = blockTexture.GetPixel(i + x * 16, 927 - y * 16 - j);
+                effectTexture.SetPixel(i, 15 - j, c);
+            }
+        }
+        effectTexture.Apply();
+        GetComponent<Renderer>().material.mainTexture = effectTexture;
     }
 }
--- client/Assets/Scripts/LogicSystem/Chunk/ChunkMeshGenerator.cs
+++ client/Assets/Scripts/LogicSystem/Chunk/ChunkMeshGenerator.cs
@@ -9,64 +9,64 @@ public static class ChunkMeshGenerator
     {
         public bool isTransparent;
         public bool isCollidable;
-        public Vector2 front;
-        public Vector2 right;
-        public Vector2 left;
-        public Vector2 back;
-        public Vector2 top;
-        public Vector2 bottom;
+        public Vector2Int front;
+        public Vector2Int right;
+        public Vector2Int left;
+        public Vector2Int back;
+        public Vector2Int top;
+        public Vector2Int bottom;
     }
 
     //方块图集的行数和列数
     static readonly int atlas_row = 58;
     static readonly int atlas_column = 24;
-
+    
     //左上为0，0
-    static Vector2 grass_bottom = new Vector2Int(2, 0);
-    static Vector2 grass_side = new Vector2Int(3, 0);
-    static Vector2 grass_top = new Vector2Int(0, 0);
+    public static Vector2Int grass_bottom = new Vector2Int(2, 0);
+    public static Vector2Int grass_side = new Vector2Int(3, 0);
+    public static Vector2Int grass_top = new Vector2Int(0, 0);
 
-    static Vector2 uv_stone = new Vector2Int(1, 0);
+    public static Vector2Int uv_stone = new Vector2Int(1, 0);
 
-    static Vector2 brick = new Vector2Int(7, 0);
+    public static Vector2Int brick = new Vector2Int(7, 0);
 
-    static Vector2 tnt_side = new Vector2Int(8, 0);
-    static Vector2 tnt_top = new Vector2Int(9, 0);
-    static Vector2 tnt_bottom = new Vector2Int(10, 0);
+    public static Vector2Int tnt_side = new Vector2Int(8, 0);
+    public static Vector2Int tnt_top = new Vector2Int(9, 0);
+    public static Vector2Int tnt_bottom = new Vector2Int(10, 0);
 
-    static Vector2 furnace_front = new Vector2Int(12, 2);
-    static Vector2 furnace_side = new Vector2Int(13, 2);
-    static Vector2 furnace_top = new Vector2Int(14, 3);
+    public static Vector2Int furnace_front = new Vector2Int(12, 2);
+    public static Vector2Int furnace_side = new Vector2Int(13, 2);
+    public static Vector2Int furnace_top = new Vector2Int(14, 3);
 
-    static Vector2 hay_side = new Vector2Int(16, 13);
-    static Vector2 hay_top = new Vector2Int(17, 13);
+    public static Vector2Int hay_side = new Vector2Int(16, 13);
+    public static Vector2Int hay_top = new Vector2Int(17, 13);
 
-    static Vector2 leaves_side = new Vector2Int(4, 12);
+    public static Vector2Int leaves_side = new Vector2Int(4, 12);
 
-    static Vector2 uv_grass = new Vector2Int(8, 2);
+    public static Vector2Int uv_grass = new Vector2Int(8, 2);
 
-    static Vector2 uv_bedrock = new Vector2Int(1, 1);
-    static Vector2 uv_poppy = new Vector2Int(12, 0);
-    static Vector2 uv_dandelion = new Vector2Int(13, 0);
-    static Vector2 uv_oakwood_side = new Vector2Int(4, 1);
-    static Vector2 uv_oakwood_top = new Vector2Int(5, 1);
-    static Vector2 uv_oakleaves = new Vector2Int(4, 3);
+    public static Vector2Int uv_bedrock = new Vector2Int(1, 1);
+    public static Vector2Int uv_poppy = new Vector2Int(12, 0);
+    public static Vector2Int uv_dandelion = new Vector2Int(13, 0);
+    public static Vector2Int uv_oakwood_side = new Vector2Int(4, 1);
+    public static Vector2Int uv_oakwood_top = new Vector2Int(5, 1);
+    public static Vector2Int uv_oakleaves = new Vector2Int(4, 3);
 
-    static TexCoords Coords_None = new TexCoords();
-    static TexCoords Coords_Dirt = new TexCoords { isTransparent = false, isCollidable = true, front = grass_bottom, right = grass_bottom, left = grass_bottom, back = grass_bottom, top = grass_bottom, bottom = grass_bottom };
-    static TexCoords Coords_GrassBlock = new TexCoords { isTransparent = false, isCollidable = true, front = grass_side, right = grass_side, left = grass_side, back = grass_side, top = grass_top, bottom = grass_bottom };
-    static TexCoords Coords_Tnt = new TexCoords { isTransparent = false, isCollidable = true, front = tnt_side, right = tnt_side, left = tnt_side, back = tnt_side, top = tnt_top, bottom = tnt_bottom };
-    static TexCoords Coords_Brick = new TexCoords { isTransparent = false, isCollidable = true, front = brick, right = brick, left = brick, back = brick, top = brick, bottom = brick };
-    static TexCoords Coords_Furnace = new TexCoords { isTransparent = false, isCollidable = true, front = furnace_front, right = furnace_side, left = furnace_side, back = furnace_side, top = furnace_top, bottom = furnace_top };
-    static TexCoords Coords_HayBlock = new TexCoords { isTransparent = false, isCollidable = true, front = hay_side, right = hay_side, left = hay_side, back = hay_side, top = hay_top, bottom = hay_top };
-    static TexCoords Coords_Leaves = new TexCoords { isTransparent = false, isCollidable = true, front = leaves_side, right = leaves_side, left = leaves_side, back = leaves_side, top = leaves_side, bottom = leaves_side };
-    static TexCoords Coords_Grass = new TexCoords { isTransparent = true, isCollidable = false, front = uv_grass };
-    static TexCoords Coords_Stone = new TexCoords { isTransparent = false, isCollidable = true, front = uv_stone, right = uv_stone, left = uv_stone, back = uv_stone, top = uv_stone, bottom = uv_stone };
-    static TexCoords Coords_BedRock = new TexCoords { isTransparent = false, isCollidable = true, front = uv_bedrock, right = uv_bedrock, left = uv_bedrock, back = uv_bedrock, top = uv_bedrock, bottom = uv_bedrock };
-    static TexCoords Coords_Poppy = new TexCoords { isTransparent = true, isCollidable = false, front = uv_poppy };
-    static TexCoords Coords_Dandelion = new TexCoords { isTransparent = true, isCollidable = false, front = uv_dandelion };
-    static TexCoords Coords_OakWood = new TexCoords { isTransparent = false, isCollidable = true, front = uv_oakwood_side, right = uv_oakwood_side, left = uv_oakwood_side, back = uv_oakwood_side, top = uv_oakwood_top, bottom = uv_oakwood_top };
-    static TexCoords Coords_OakLeaves = new TexCoords { isTransparent = true, isCollidable = true, front = uv_oakleaves, right = uv_oakleaves, left = uv_oakleaves, back = uv_oakleaves, top = uv_oakleaves, bottom = uv_oakleaves };
+    public static TexCoords Coords_None = new TexCoords();
+    public static TexCoords Coords_Dirt = new TexCoords { isTransparent = false, isCollidable = true, front = grass_bottom, right = grass_bottom, left = grass_bottom, back = grass_bottom, top = grass_bottom, bottom = grass_bottom };
+    public static TexCoords Coords_GrassBlock = new TexCoords { isTransparent = false, isCollidable = true, front = grass_side, right = grass_side, left = grass_side, back = grass_side, top = grass_top, bottom = grass_bottom };
+    public static TexCoords Coords_Tnt = new TexCoords { isTransparent = false, isCollidable = true, front = tnt_side, right = tnt_side, left = tnt_side, back = tnt_side, top = tnt_top, bottom = tnt_bottom };
+    public static TexCoords Coords_Brick = new TexCoords { isTransparent = false, isCollidable = true, front = brick, right = brick, left = brick, back = brick, top = brick, bottom = brick };
+    public static TexCoords Coords_Furnace = new TexCoords { isTransparent = false, isCollidable = true, front = furnace_front, right = furnace_side, left = furnace_side, back = furnace_side, top = furnace_top, bottom = furnace_top };
+    public static TexCoords Coords_HayBlock = new TexCoords { isTransparent = false, isCollidable = true, front = hay_side, right = hay_side, left = hay_side, back = hay_side, top = hay_top, bottom = hay_top };
+    public static TexCoords Coords_Leaves = new TexCoords { isTransparent = false, isCollidable = true, front = leaves_side, right = leaves_side, left = leaves_side, back = leaves_side, top = leaves_side, bottom = leaves_side };
+    public static TexCoords Coords_Grass = new TexCoords { isTransparent = true, isCollidable = false, front = uv_grass };
+    public static TexCoords Coords_Stone = new TexCoords { isTransparent = false, isCollidable = true, front = uv_stone, right = uv_stone, left = uv_stone, back = uv_stone, top = uv_stone, bottom = uv_stone };
+    public static TexCoords Coords_BedRock = new TexCoords { isTransparent = false, isCollidable = true, front = uv_bedrock, right = uv_bedrock, left = uv_bedrock, back = uv_bedrock, top = uv_bedrock, bottom = uv_bedrock };
+    public static TexCoords Coords_Poppy = new TexCoords { isTransparent = true, isCollidable = false, front = uv_poppy };
+    public static TexCoords Coords_Dandelion = new TexCoords { isTransparent = true, isCollidable = false, front = uv_dandelion };
+    public static TexCoords Coords_OakWood = new TexCoords { isTransparent = false, isCollidable = true, front = uv_oakwood_side, right = uv_oakwood_side, left = uv_oakwood_side, back = uv_oakwood_side, top = uv_oakwood_top, bottom = uv_oakwood_top };
+    public static TexCoords Coords_OakLeaves = new TexCoords { isTransparent = true, isCollidable = true, front = uv_oakleaves, right = uv_oakleaves, left = uv_oakleaves, back = uv_oakleaves, top = uv_oakleaves, bottom = uv_oakleaves };
 
     public static TexCoords[] type2texcoords = new TexCoords[15]
     {
